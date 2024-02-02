cc3d4b673566ccb242808440d2d67937839b03f7
Franklin Foko <fokofranklin47@gmail.com>
2024-02-02 01:41:04 +0100

    ok

--- aks/main.tf
+++ aks/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     admin_username = var.username
 
     ssh_key {
-      key_data = jsondecode(azapi_resource_acrtion.ssh_public_key_gen.output).publicKey
+      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
     }
   }
   network_profile {
